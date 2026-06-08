44d121a8cd463758e014e5ecde65b9d7c0990249
Hưng Đinh <hungthkk123@gmail.com>
2026-06-08 10:31:20 +0700

    Ex8 SS9

--- Ex10.sql
+++ Ex10.sql
@@ -1,28 +1,30 @@
-select *from newcustomers
-union
-select *from oldcustomers;
+create or replace procedure add_order_and_update_customer(
+    p_customer_id INT,
+    p_amount NUMERIC
+) language plpgsql
+as $$
+    declare
+        check_exist int;
+    begin
+        select customer_id into check_exist from customers
+        where p_customer_id=customer_id;
 
-select *from newcustomers
-intersect
-select *from oldcustomers;
+        if check_exist is null then
+            raise exception 'Lỗi không tồn tại khách hàng';
+        end if;
 
-select city, count(id) from (
-    select *from newcustomers
-    union
-    select *from oldcustomers
-) group by city;
+        insert into orders (customer_id, total_amount) values
+        (p_customer_id,p_amount);
 
-select city, count(id) from (
-    select *from newcustomers
-    union
-    select *from oldcustomers
-) group by city having count(id) = (
-    select count(id) from (
-        select *from newcustomers
-        union
-        select *from oldcustomers
-        )group by city
-    order by count(id) desc limit 1
-);
+        update customers
+        set total_spent=total_spent+p_amount
+        where customer_id=p_customer_id;
 
+    exception
+        when others then
+            raise notice 'Xảy ra lỗi: %',SQLERRM;
+        rollback;
+    end;
+$$;
 
+call add_order_and_update_customer(1,50);
